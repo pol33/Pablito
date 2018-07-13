c2bef3851ba726c1cd0631c9beb8629972223389
pol33 <36174687+pol33@users.noreply.github.com>
2018-07-12 21:22:34 -0300

    Add files via upload

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,13 +1,28 @@
-
+function mostrar()
+{
 	var res;
 	var numero;
 	var mayor;
 	var menor;
 	var aux="0";
+	var suma=0;
+	var numPares=0;
 	do{
+
 		numero=prompt("Escriba un numero");
 		numero=parseInt(numero);
-	
+
+		while(isNaN(numero)){
+			numero=prompt("Incorrecto");
+        	numero=parseInt(numero);
+        }
+
+        if(numero%2==0){
+        	numPares++;
+        }
+
+        suma=suma+numero;
+
 		if(numero>mayor||aux=="0"){
 		mayor=numero;
 		}
@@ -20,7 +35,10 @@
 
   	}while(res=="s");
 
-	alert("El mayor es "+mayor+" y el menor es "+menor);
+	alert("El mayor es "+mayor+" y el menor es "+menor+" y "+numPares+"La suma es "+suma);
 
-
-
+}
+function Mensaje()
+{
+	alert("Te estoy observando ANA!!!");
+}
